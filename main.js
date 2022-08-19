36d7c38c24afc8962417b4948078a7fcab49b012
AmegaPN <95965729+AmegaPN@users.noreply.github.com>
2022-08-19 11:10:32 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/2XU0tgf9-/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+
 var dog = 0;
 var cat = 0;
 var cow = 0;
